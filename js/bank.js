a8bd0aeec6365c2071bc9c8dd24ce8f5f7f37135
shajib2 <shajibk424@gmail.com>
2022-02-10 01:24:24 +0600

    add another js

--- js/bank.js
+++ js/bank.js
@@ -4,6 +4,6 @@ document.getElementById("login-submit").addEventListener("click", function () {
     const passwordField = document.getElementById("user-password");
     const useaPassword = passwordField.value;
     if (useaEmail == "sontan@baap.com" && useaPassword == "secret") {
-        console.log("valid user")
+        window.location.href = "banking.html"
     }
 })
